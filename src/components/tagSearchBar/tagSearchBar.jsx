15c844b724060694f3a4b5d3b0405f965ade370e
codesmitty <varcodesmitty@gmail.com>
2021-01-05 15:16:57 -0500

    working on clear button

--- src/components/tagSearchBar/tagSearchBar.jsx
+++ src/components/tagSearchBar/tagSearchBar.jsx
@@ -5,18 +5,20 @@ import deleteLogo from "../../images/icon-remove.svg";
 const TagSearchBar = ({ filters, handleDeleteFilters }) => {
   return (
     <div className="tag-search-container">
-      {filters.map((tag) => (
-        <div className="filters-container">
-          <div className="filters">{tag}</div>
-          <button onClick={() => handleDeleteFilters(tag)} className="x">
-            <img
-              alt="remove-filter-icon"
-              className="remove-icon"
-              src={deleteLogo}
-            />
-          </button>
-        </div>
-      ))}
+      <div className="filter-wrapper">
+        {filters.map((tag) => (
+          <div className="filters-container">
+            <div className="filters">{tag}</div>
+            <button onClick={() => handleDeleteFilters(tag)} className="x">
+              <img
+                alt="remove-filter-icon"
+                className="remove-icon"
+                src={deleteLogo}
+              />
+            </button>
+          </div>
+        ))}
+      </div>
       <button className="clear-button">clear</button>
     </div>
   );
